77b93b5a64982576db67cd33ec9bd3018944f889
Mauricio Serrato <mauricioserrato@Mauricios-MacBook-Pro.local>
2022-08-31 09:39:56 -0500

    changes made to show button, inserted off canvas

--- src/components/NavComponent.js
+++ src/components/NavComponent.js
@@ -2,7 +2,6 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
 const ComicNav = (props) => {
     const showNewForm = () => {
         props.setNewForm(!props.showNewForm)
@@ -24,17 +23,9 @@ const ComicNav = (props) => {
                     <Nav className="me-auto">
                         <Nav.Link href="#home" onClick={showCollection}>Collections</Nav.Link>
                         <Nav.Link href="#link" onClick={showNewForm}>Add New Comic</Nav.Link>
-                        <NavDropdown title="Links" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Instagram</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Facebook
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Twitter</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Marvel
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="#link" onClick={showNewForm}>Cart: {props.cart}</Nav.Link>
+
+                        
                     </Nav>
                 </Navbar.Collapse>
             </Container>
